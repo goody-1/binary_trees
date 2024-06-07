84035eddc831341327014dba8af6f434a233dab4
goody-1 <atundeg@gmail.com>
2024-06-07 03:39:26 +0100

    write function that does post-order transversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder- goes through a binary tree using post-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node
+ *
+ * Return: void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	/* Traverse the left subtree */
+	binary_tree_postorder(tree->left, func);
+
+	/* Traverse the right subtree */
+	binary_tree_postorder(tree->right, func);
+
+	/* Process the current node */
+	func(tree->n);
+}
